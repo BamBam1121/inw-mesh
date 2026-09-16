21e4436f6324c64b6f26f6c37ef8badcb6684d3b
BamBam1121 <93291182+BamBam1121@users.noreply.github.com>
2026-09-16 15:24:42 -0700

    Laptop watchdog: check every minute

--- web/watchdog.laptop.cmd
+++ web/watchdog.laptop.cmd
@@ -1,4 +1,4 @@
 @echo off
-rem Starts the website again if caddy is not running. Runs every 5 minutes as SYSTEM.
+rem Starts the website again if caddy is not running. Runs every minute as SYSTEM.
 tasklist /fi "imagename eq caddy.exe" | find /i "caddy.exe" >nul
 if errorlevel 1 schtasks /run /tn "Squatch Mesh website" >nul
